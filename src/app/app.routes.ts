fabe373ee1be53ba777cfc3532c091fcbea1500d
KholoudIbrahim <kholoud32csfue@gmail.com>
2025-08-03 10:24:43 +0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { BannerComponent } from './components/banner/banner.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: BannerComponent },
+  { path: 'home', component: HomepageComponent },
+  { path: '**', redirectTo: '' },
+];
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+// https://www.builder.io/c/docs/using-your-api-key
+export const environment = {
+  production: false,
+  builderApiKey: "9e73ed29f737446f863ad1d357983a5f",
+};
